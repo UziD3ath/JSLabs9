301095476473cedd74115f6ea26861a037a9da7c
UziD3ath <86554997+UziD3ath@users.noreply.github.com>
2025-01-15 15:47:25 +0500

    Final touches

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ var button = document.getElementById("entrybutton);
  
 function displayText(){
   var text =document.getElementById("entryinput");
-  document.getElementById("textoutput").innerHTML =
+  document.getElementById("textoutput").innerHTML = text.value;
+  alert("Uzair says: " + text.value);
+}
 
-
-
-
-
+  button.addEventListener("click", displayText);
+}
 
 window.addEventListener('load', init);
